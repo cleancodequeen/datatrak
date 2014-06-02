793b8f65918e56f98d67f30df23e8ca2b3d62913
Andrew Nida <andrewnida@Andrews-MacBook-Pro.local>
2014-06-02 03:38:11 -0400

    single mirrors page

--- wp-content/themes/datatrak/single.php
+++ wp-content/themes/datatrak/single.php
@@ -23,8 +23,49 @@ get_header(); ?>
 
 <?php the_breadcrumb(); ?>
 
-<div id="page">
-i'm a the single!
+<?php 
+
+	if ( ( get_post_meta( get_the_ID(), 'cta_1_label', true ) && get_post_meta( get_the_ID(), 'cta_1_url', true ) ) ||
+		 ( get_post_meta( get_the_ID(), 'cta_2_label', true ) && get_post_meta( get_the_ID(), 'cta_2_url', true ) ) || 
+		 ( get_post_meta( get_the_ID(), 'cta_3_label', true ) && get_post_meta( get_the_ID(), 'cta_3_url', true ) ) ) {
+
+		$hasCTA = true;
+	} else {
+		$hasCTA = false;
+	}
+
+	$copyClass = $hasCTA ? 'partial' : 'full';
+?>
+
+<div id="page" class="content group">
+	<?php if( have_posts() ) : ?>
+		<div class="copy <?php echo $copyClass; ?>">
+		<?php while( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'loop-header' ); ?>
+
+			<div id="post-<?php the_ID(); ?>" <?php post_class('page-content'); ?>>
+				
+				<?php the_content( __( 'Read more &#8250;', 'datatrak' ) ); ?>
+
+			</div><!-- end of #post-<?php the_ID(); ?> -->
+
+		<?php
+		endwhile;
+		?>
+		</div>
+		<?php 
+			if ($hasCTA) {
+				get_template_part( 'loop-sidebar' );
+			} 
+		?>
+	<?php
+	else :
+
+		//get_template_part( 'loop-no-posts' );
+
+	endif;
+	?>
 </div>
 
 <?php get_footer(); ?>
